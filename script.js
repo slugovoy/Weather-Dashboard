d3c31f93e97738b0a1ba7db0bd718ce332e9476f
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-11-14 07:05:28 -0700

    added functions and variables

--- script.js
+++ script.js
@@ -8,15 +8,22 @@ function buildURL() {
 
   return queryURL + $.param(queryParams);
 }
-let currentDate = luxon.DateTime.local().toFormat("D");
+//let currentDate = luxon.DateTime.local().toFormat("D");
 $("#startSearch").on("click", function (e) {
   e.preventDefault();
+
+  $("#cityInfo").empty();
+  $("#day1").empty();
+  $("#day2").empty();
+  $("#day3").empty();
+  $("#day4").empty();
+  $("#day5").empty();
+
   let queryURL = buildURL();
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (data) {
-    console.log(queryURL);
     let city = $("<h3>").text(data.name + "(" + currentDate + ")");
     let F = Math.floor((data.main.temp - 273.15) * 1.8 + 32);
     let tempNow = $("<p>").text("Temperature: " + F + " F");
@@ -24,39 +31,70 @@ $("#startSearch").on("click", function (e) {
     let windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
     let indexUV = $("<p>").text("UV Index: ");
     $("#cityInfo").append(city, tempNow, humidNow, windSpeed);
-    $("#citiesNames").append($("<h5>").text(data.name));
+    $("#citiesNames").append($("<h5>").attr("class", "searchHistory").text(data.name));
 
     // Second call
 
     function buildSecondURL() {
-        let querySecondURL = "https://api.openweathermap.org/data/2.5/onecall?";
-        let querySecondParams = { appid: "b822a69b22827a30f03ca2082b6ba3a1" };
-      
-        querySecondParams.lat = data.coord.lat;
-        querySecondParams.lon = data.coord.lon;
-        console.log(querySecondParams);
-      
-      
-        return querySecondURL + $.param(querySecondParams);
+      let querySecondURL = "https://api.openweathermap.org/data/2.5/onecall?";
+      let querySecondParams = { appid: "b822a69b22827a30f03ca2082b6ba3a1" };
+
+      querySecondParams.lat = data.coord.lat;
+      querySecondParams.lon = data.coord.lon;
+      console.log(querySecondParams);
+
+      return querySecondURL + $.param(querySecondParams);
+    }
+
+    let secondQueryURL = buildSecondURL();
+    $.ajax({
+      url: secondQueryURL,
+      method: "GET",
+    }).then(function (response) {
+      let UVI = response.current.uvi;
+      let indexUV = $("<p>").text("UV Index: " + UVI);
+      $("#cityInfo").append(indexUV);
+      if (UVI <= 3){
+          indexUV.addClass("green");
+      }else if(UVI > 3 && UVI < 6){
+        indexUV.addClass("yellow");
+      }else if(UVI > 6 && UVI < 8){
+        indexUV.addClass("orange");
+      }else if(UVI > 8 && UVI < 11){
+        indexUV.addClass("red");
+      }else{
+        indexUV.addClass("violet");
       }
 
-      let secondQueryURL = buildSecondURL();
-      $.ajax({
-          url: secondQueryURL,
-          method: "GET"
-      }).then(function(response){
-          console.log(secondQueryURL);
-          let indexUV = $("<p>").text("UV Index: " + response.current.uvi); 
-          $("#cityInfo").append(indexUV);
+      for (let i = 1; i < 6; i++) {
+        let F1 = Math.floor((response.daily[i].temp.day - 273.15) * 1.8 + 32);
 
+        $("#day" + [i]).append($("<h6>").text(currentDate + [i]));
+        let condition = response.daily[i].weather[0].main;
+        //$("#day"+[i]).append($("<p>").attr("class", "pForForecast").text(condition));
+        if (condition == "Clear") {
+          $("#day" + [i]).append($("<i>").attr("class", "fas fa-sun"));
+        } else if (condition == "Rain") {
+          $("#day" + [i]).append(
+            $("<i>").attr("class", "fas fa-cloud-showers-heavy")
+          );
+        } else if (condition == "Clouds") {
+          $("#day" + [i]).append($("<i>").attr("class", "fas fa-cloud"));
+        }
 
-          for (let i = 1; i < 6; i++) {
-              $("#cityForecast").append($("<h4>").text(currentDate + [i]))
-              
-          }
+        $("#day" + [i]).append(
+          $("<p>")
+            .attr("class", "pForForecast")
+            .text("Temp.: " + F1 + "F")
+        );
 
-      })
-
-
+        $("#day" + [i]).append(
+          $("<p>")
+            .attr("class", "pForForecast")
+            .text("Humidity: " + response.daily[i].humidity + "%")
+        );
+      }
+      
+    });
   });
 });
